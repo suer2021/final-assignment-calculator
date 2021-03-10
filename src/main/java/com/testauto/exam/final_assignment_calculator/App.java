75aab9c5400cc37f94965d2d5c0e9c6f8968d01a
Susanne Erixon <UZAB@RSV.SE>
2021-03-10 10:27:41 +0100

    First commit. Just started assignment project

---
+++ src/main/java/com/testauto/exam/final_assignment_calculator/App.java
@@ -0,0 +1,13 @@
+package com.testauto.exam.final_assignment_calculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/testauto/exam/final_assignment_calculator/AppTest.java
@@ -0,0 +1,20 @@
+package com.testauto.exam.final_assignment_calculator;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
